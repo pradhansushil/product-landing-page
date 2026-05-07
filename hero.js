d00c3b3644063fe5ae70d5c7979da639a15852af
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-05-07 05:55:33 +0545

    feat: add autoplay slideshow with dot sync and arrow timer reset

--- hero.js
+++ hero.js
@@ -1,10 +1,21 @@
 import getDOMElements from "./dom-elements.js";
 
+let pauseSlide;
+
+function currentDot(dom, index) {
+  dom.dots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+  dom.dots[index].classList.add("active");
+}
+
 function goToSlide(dom, index) {
   dom.slides.forEach((slide) => {
     slide.classList.remove("active");
   });
   dom.slides[index].classList.add("active");
+
+  currentDot(dom, index);
 }
 
 function initHero() {
@@ -14,6 +25,17 @@ function initHero() {
 
   let currentSlide = 0;
   goToSlide(dom, 0);
+
+  function startSlide() {
+    pauseSlide = setInterval(() => {
+      currentSlide = (currentSlide + 1) % dom.slides.length;
+      goToSlide(dom, currentSlide);
+    }, 5000);
+  }
+
+  function clearTimer() {
+    clearInterval(pauseSlide);
+  }
 
   dom.swatches.forEach((swatch) => {
     swatch.addEventListener("click", () => {
@@ -25,10 +47,18 @@ function initHero() {
   dom.arrows[0].addEventListener("click", () => {
     currentSlide = (currentSlide - 1 + dom.slides.length) % dom.slides.length;
     goToSlide(dom, currentSlide);
+    clearTimer();
+    startSlide();
   });
 
   dom.arrows[1].addEventListener("click", () => {
     currentSlide = (currentSlide + 1) % dom.slides.length;
     goToSlide(dom, currentSlide);
+    clearTimer();
+    startSlide();
   });
+
+  startSlide();
 }
+
+initHero();
