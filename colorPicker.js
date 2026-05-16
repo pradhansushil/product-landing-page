df51dd6425bfa5e13d46b0b8dcdb0074acd0a2f2
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-05-17 03:08:08 +0545

    fix: call onSelect for pre-selected swatch on color picker init

--- colorPicker.js
+++ colorPicker.js
@@ -1,5 +1,10 @@
 export default function initColorPicker(container, attributeName, onSelect) {
   container.swatches.forEach((swatch) => {
+    if (swatch.classList.contains("selected")) {
+      const value = swatch.getAttribute(attributeName);
+      onSelect(value);
+    }
+
     swatch.addEventListener("click", () => {
       container.swatches.forEach((s) => s.classList.remove("selected"));
       swatch.classList.add("selected");
